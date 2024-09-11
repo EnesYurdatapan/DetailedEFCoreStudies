628045d0f50971ab2264a4581b4b97afc778a572
Enes Yurdatapan <enesyurdatapan@hotmail.com>
2024-09-11 16:15:16 +0300

    11.09.2024 / Backing Fields & Shadow Properties

---
+++ Lesson11.BackingFields/Lesson11.BackingFields/Program.cs
@@ -0,0 +1,90 @@
+﻿
+
+#region Backing Fields
+
+// Tablo içerisindeki kolonları, entity class'ları içinde propertyler ile değil fieldlarla temsil etmemizi sağlayan bir özelliktir.
+// Veritabanından bize gelen verileri kapsülleyerek dış dünyaya açabilir, dış dünyadan da veritabanına kapsülleyerek veri yollayabiliriz.
+using Microsoft.EntityFrameworkCore;
+
+class Person
+{
+    public int Id { get; set; }
+    public string name;
+    public string Name { get => name; set => name = value; }
+ // public string Name { get => name.Substring(0,3); set => name = value; }  ***Kapsülleme örneği
+    public string Department { get; set; }
+}
+
+#endregion
+
+
+#region BackingField Attributes
+
+// Attribute ile backingfield
+
+class Person2
+{
+    public int Id { get; set; }
+    public string name;
+    [BackingField(nameof(name))]
+    public string Name { get => name; set => name = value; }
+    public string Department { get; set; }
+}
+
+
+#endregion
+
+
+#region HasField FluentAPI
+
+//protected override void OnModelCreating(ModelBuilder modelBuilder)
+//{
+//    modelBuilder.Entity<Person>()
+//        .Property(p=>p.Name)
+//        .HasField(nameof(Person.name));
+//}
+
+
+#endregion
+
+
+#region Field and Property Access
+//Ef Core sorgulama sürecinde entity içerisindeki property veya fieldları kullanıp kullanılmayacağının davranışını bizlere belirtmektedir.
+
+//EF Core, hiçbir ayarlama yoksa varsayılan olarak propertyler üzerinden verileri işler. Eğer ki backing field bildiriliyorsa field üzerinden işler veya nasıl ayarlandıysa öyle işler.
+
+
+//UsePropertyAccessMode üzerinden davranış modellemesi gerçekleştirilebilir.
+
+
+//protected override void OnModelCreating(ModelBuilder modelBuilder)
+//{
+//    modelBuilder.Entity<Person>()
+//        .Property(p=>p.Name)
+//        .HasField(nameof(Person.name))
+//        .UsePropertyAccessMode(PropertyAccessMode.Field); // Veri erişim sürecinde saece fieldın kullanılmasını söyler. Eğer field'ın kullanılamayacağı durum mevcutsa exception fırlatır.
+//}
+
+#endregion
+
+
+#region Field-Only Properties
+
+//Entitylerde değerleri almak için properrtyler yerine metotların kullanıldığı veya belirli alanların hiç gösterilmemesi gerektiği durumlarda(örneğin primary key kolonu) kullanılabilir.
+
+class Person3
+{
+    public int Id { get; set; }
+    public string name;
+    public string Department { get; set; }
+
+    //public string GetName()
+    //    => name;
+    //public string SetName(string value)
+    //    =>this.name = value;   >>> opsiyonel yapabildiğimiz işlemler
+}
+
+// modelBuilder.Entity<Person>()
+// .Property(nameof(Person.name));  name field'ına bir property muamelesi yapması gerektiğini bildirdik.
+
+#endregion
---
+++ Lesson12.ShadowProperties/Lesson12.ShadowProperties/Program.cs
@@ -0,0 +1,71 @@
+﻿#region Shadow Properties - Gölge Özellikler
+//Entity sınıflarında fiziksel olarak tanımlanmayan/modellenmeyen fakat EF Core tarafından ilgili entity için var olan/var olduğu kabul edilen propertylerdir.
+
+//Tabloda gösterilmesini istediğimiz, gerekli görmediğimiz, entity instance'ı üzerinde işlem yapmayacağımız kolonlar için shadow propertyler kullanılabilir.
+
+//Shadow propetylerin değerleri ve stateleri Change Tracker tarafından kontrol edilir.
+#endregion
+
+
+#region Foreign Key - Shadow Properties
+// Shadow Properties özelliğini aslında daha önce kullandık.
+// Örneğin bire çok ilişkideki dependent entity'de foreign key tanımlamamamıza rağmen bir foreign key property'si oluşturulur. İşte bu shadow propertydir.
+
+
+
+#endregion
+
+#region Shadow Property oluşturma
+
+//Foreign key dışında bir shadow property oluşturmak için Fluent API ile yapabiliriz.
+
+//protected override void OnModelCreating(ModelBuilder modelBuilder)
+//{
+//    modelBuilder.Entity<Blog>()
+//        .Property<DateTime>("CreatedDate); Artık bir CreatedDate kolonu oluşturmuş olduk. ShadowProperty
+//}
+
+
+
+#endregion
+
+#region Shadow Property'e erişim sağlama
+
+#region ChangeTracker ile Erişim
+
+
+//var blog = await context.Blogs.FirstAsync();
+//var createdDate= context.Entry(blog).Property("CreatedDate");
+// Console.WriteLine(createdDate.CurrentValue);
+// Console.WriteLine(createdDate.OriginalValue);
+
+//createdDate.CurrentValue = DateTime.Now;
+// await context.SaveChangesAsync();
+
+#endregion
+
+#region EF.Property ile Erişim
+// Özellikle LINQ sorgularında Shadow Property'lerine erişim için EF.Property static yapılanmasını kullanabiliirz.
+
+// context.Blogs.OrderBy(b=> EF.Property<DateTime>(b,"CreatedDate")).ToListAsync();
+
+#endregion
+
+
+#endregion
+
+
+class Blog
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public ICollection<Post> Posts { get; set; }
+}
+
+class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public bool lastUpdated { get; set; }
+    public Blog Blog { get; set; }
+}
