0881403cd536efdff911e0816194e296a085492c
Enes Yurdatapan <enesyurdatapan@hotmail.com>
2024-10-14 23:57:33 +0300

    14.10.2024 / Seed Data & TPH

---
+++ Lesson16.SeedData/Lesson16.SeedData/Program.cs
@@ -0,0 +1,47 @@
+﻿#region Data Seeding Nedir ?
+//EF Core ile inşa edilen veritabanı içerisinde veritabanı nesneleri olabileceği gibi verilerin de migrate
+//sürecinde üretilmesini isteyebiliriz.
+
+//İşte bu ihtiyaca istinaden Seed Data özelliği ile EF Core üzerinden migrationlarda veriler oluşturulabilir ve migrate ederken bu verileri hedef tablolarımıza basabiliriz.
+
+//Migrate sürecinde Seed datalar migrate sürecinde hazır verileri tablolara basabilmek için bunları  yazılım kısmında tutmamızı gerektirmektedir.Böylece bu veriler üzerinde veritabanı seviyesinde istenilen manipülasyonlar gönül rahatlığıyla gerçekleştirilebilir.
+
+
+//Data Seeding özelliği şu noktalarda kullanılabilir ;
+
+// > Test için geçici verilere ihtiyaç varsa,
+// > Asp.NET Core'daki Identity yapılanmasındaki roller gibi static değerler de tutulabilir.
+// > Yazılım için temel konfigürasyonel değerler.
+#endregion
+
+
+#region Seed Data Ekleme
+//OnModelCreating metodu içerisinde Entity fonksiyonundan sonra çağırılan HasData fonksiyonu ilgili entitye karşılık Seed Dataları eklememizi sağlayan bir fonksiyondur.
+
+//PK kolonu manuel olarak verilmelidir.
+
+
+// modelBuilder.Entity<Blog>()
+//              .HasData( new Blog(){ Id=1, Url = "www.bilmemne.com"},);
+//                        new Blog(){ Id=2, Url = "www.bilmemne2.com"};
+
+#endregion
+
+
+#region İlişkisel verilerde seed data ekleme
+
+// İlişkisel senaryolarda dependent table'a veri eklerken foreign key kolonuun propertyisi varsa eğer ona ilişkisel değeri vererek ekleme işlemini yapıyoruz. Aksi takdirde hata verecktir.
+
+
+// modelBuilder.Entity<Post>()
+//              .HasData( new Post(){ Id=1, BlogId=1,Title = "A"},);
+//                        new Post(){ Id=2,BlogId=1, Title = "B"};
+
+#endregion
+
+
+#region Seed Data'nın PK'ini değiştirme
+// Eğer ki migrate edilen herhangi bir seed datanın sonrasında PK'i değiştirilirse bu datayla ilişkili veriler(varsa) cascade davranışı göstereceklerdir(yani silineceklerdir.)
+
+
+#endregion
---
+++ Lesson17.TablePerHierarchy/Lesson17.TablePerHierarchy/Program.cs
@@ -0,0 +1,104 @@
+﻿#region Table Per Hierarchy(TPH) nedir ?
+
+//Kalıtmsal ilişkiye sahip olan entitylerin olduğu senaryolarda her bir hiyerarşiye karşılık bir tablo oluşturan davranıştır.
+//İçerisinde benzer alanlara sahip olan entityleri migrate ettiğimizde her entitye karşılık bir tablo oluşturmaktansa bu entityleri tek bir tabloda modellemek isteyebilir ve bu tablodaki kayıtları discriminator kolonu üzerinden birbirleniden ayırabiliriz. İşte bu tarz bir tablonun oluşturulması ve bu tarz bir tabloya göre sorgulama, veri ekleme, silme vs. gibi operasyonların şekillendirilmesi için TPH davranışını kullanabiliriz.
+#endregion
+
+
+#region TPH Nasıl Uygulanır ?
+
+//EF Core'da entityler arasında temel bir kalıtımsal ilişki söz konusuysa, default olarak kabul edilen bir davranıştır.
+// O yüzden herhangi bir konfigürasyon gerektirmez.
+
+//Tüm entityler DbSet'e eklenmelidir.
+class Person
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
+}
+
+class Employee:Person
+{
+    public string? Department { get; set; }
+}
+
+class Techinician:Employee
+{
+    public string? Branch { get; set; }
+}
+
+
+#endregion
+
+#region Discriminator kolonu nedir ?
+//TPH yaklaşımı neticesinde kümülatif olarak inşa edilmiş tablonun hangi entitye karşılık veri tuttuğunu ayırt edebilmemizi sağlayan bir kolondur.
+//EF Core tarafından otomatik olarak tabloya yerleştirilier.
+// Default olarak içerisinde entity isimlerini tutar.
+//Bu kolon özelleştirilebilir.
+
+
+#endregion
+
+#region Discriminator kolon adı nasıl değiştirilir ?
+
+//Öncelikle hiyerarşinin başında hangi tablo varsa onun Fluent API'da konfigürasyonuna gidilir.
+// Ardından HasDiscriminator fonksiyonu ile özelleştirilir.
+
+//.HasDiscriminator<string>("ayırıcı");
+
+#endregion
+
+#region Discriminator Değerleri Nasıl Değiştirilir ?
+//Yine hiyerarşi başındaki entity konfigürasyonlarına gelip, HasDiscriminator fonksiyonu ile özelleştirmede bulunarak ardından HasValue ile hangi entitye karşılık hangi değerin girileceğini belirtilen türde veririrz.
+//Örneğin int yaparsak aşağıdaki gibi belirtilir; bunun yanında string veya başka türdeki değerleri de aynı şekilde  HasValue ile verebiliriz.
+
+//.HasDiscriminator<int>("ayırıcı");
+//.HasValue<Person>(1)
+//.HasValue<Employee>(2)
+//.HasValue<Customer>(3)
+//.HasValue<Technician>(4)
+
+#endregion
+
+
+#region TPH'da veri ekleme
+
+//Davranışların hiçbirinde veri eklerken,silerken,güncellerken vs. normal operasyonların dışında bir işlem yapılmaz!
+
+//Hangi davranışı kullanıyorsak EF Core ona göre arka planda modellemeyi gerçekleştirir.
+
+//Employee employee = new Employee(){ ...... } ; 
+//Technician technician = new Technician(){ ...... } ; 
+
+// await context.Employee.AddAsync(employee);
+// await context.Technician.AddAsync(technician);
+
+#endregion
+
+#region TPH'de Veri silme
+
+//Yine aynı şekilde kendi entitysi üzerinden silme operasyonu gerçekleşir.
+
+//var employee = context.Employee.FindAsync(1);
+// await context.Employees.Remove(employee);
+
+#endregion
+
+#region TPH'de veri güncelleme
+
+//yine aynı şekilde Entity üzerinden yapılır.
+
+#endregion
+
+#region TPH'de veri sorgulama
+
+//Veri sorgulama operasyonu bilinen DbSet propertysi üzerinden sorgulanır. Ancak burada dikkat edilmesi gereken husus ; kalıtımsal ilişkiye göre yapılan sorgulamada üst sınıf alt sınıftaki verileri de kapsamaktadır. Bu yüzden alt sınıfa ait veriler de gelecektir.
+//Sorgulama süreçlerinde EF Core generate edilen sorguya bir Where davranışı eklemektedir.
+
+#endregion
+
+#region Farklı Entitylerde aynı isimde sütun olduğu durumlar
+//Entitylerde aynı isimde kolonlar olabilir. Bu kolonları EF Core isimsel olarak özelleştirip ayıracaktır.
+
+#endregion
