416006862145e3408e6b7c8047cd52a977ea328a
Enes Yurdatapan <enesyurdatapan@hotmail.com>
2024-10-16 17:01:25 +0300

    16.10.2024 / TPT && TPC

---
+++ Lesson18.TablePerType/Lesson18.TablePerType/Program.cs
@@ -0,0 +1,69 @@
+﻿#region Table Per Type (TPT) Nedir ?
+
+//Entitylerin arasında kalıtımsal ilişkiye sahip olduğu durumlarda her bir entitye karşılık bir tablo generate eden davranıştır.
+// Her generate edilen tablo hiyerarşik düzlemde kendi aralarında birebir ilişkiye sahiptir.
+
+#endregion
+
+class Person
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
+}
+
+class Employee : Person
+{
+    public string? Department { get; set; }
+}
+
+class Techinician : Employee
+{
+    public string? Branch { get; set; }
+}
+
+#region TPT nasıl uygulanır ?
+
+//TPT'yi uygulayabilme için öncelikle entityler kendi aralarında olması gereken mantıkta inşaa edilir.
+// Entityler DbSet olarak bildirilmelidir.
+//Hiyerarşik olarak aralarında kalıtımsal ilişki olan tüm entityler OnModelCreating fonksiyonunda ToTable metodu ile konfigüre edilmelidir.
+
+
+// modelBuilder.Entity<Person>().ToTable("Persons");
+// modelBuilder.Entity<Employee>().ToTable("Employees");
+// modelBuilder.Entity<Technician>().ToTable("Technician");
+
+#endregion
+
+
+#region TPT'de veri ekleme
+namespace example
+{
+Techinician techinician = new() { Name ="Enes", Surname="Yurdatapan", Department="Yazılım",Branch="Kodlama" }
+//await context.Technicians.AddAsync(technician);
+//await context.SaveChangesAsync();
+}
+
+#endregion
+
+
+#region TPT'de veri silme
+
+//Employee silinecek = await context.Employees.FindAsync(3);
+//await context.Employees.Remove(silinecek);
+//await context.SaveChangesAsync();
+#endregion
+
+#region TPT'de veri güncelleme
+
+// Veriyi dağıttığı için herhangi bir yerden yakalayıp güncelleme yapılabilir.
+
+//Technician technician = await context.Technicians.FindAsync(2);
+// technician.Name="Ahmet";
+//await context.Technicians.UpdateAsync(technician);
+// await context.SaveChangesAsync();
+
+
+#endregion
+
+
---
+++ Lesson19.TablePerConcreteType/Lesson19.TablePerConcreteType/Program.cs
@@ -0,0 +1,20 @@
+﻿#region Table Per Concrete Type (TPC) Nedir?
+
+//TPC davranışı, kalıtımsal ilişkiye sahip olan entitylerin olduğu çalışmalarda sadece concrete olan entitylere karşılık bir tablo oluşturacak davranış modelidir.
+//TPC, TPT'nin daha performanslı versiyonudur.
+// TPT gibi bire bir ilişki bulunmaz. Her tabloda kalıtım aldığı entitynin kolonları bulunur.
+#endregion
+
+#region TPC nasıl uygulanır ?
+
+//Hiyerarşik düzlemde abstract olan yapılar üzerinden OnModelCreating Entity fonksiyonuyla konfigürasyona girip ardından UseTpcMappingStrategy fonksiyonu eşliğinde davranışın TPC olacağını belirleyebiliriz.
+
+// modelBuilder.Entity<Person>().UseTpcMappingStrategy();
+
+#endregion
+
+#region TPC'de veri ekleme, silme, güncelleme, sorgulama
+
+// CRUD işlemlerinde TPH,TPT'de de olduğu gibi normal süreçten hiçbir fark yoktur.
+
+#endregion
