a21e856cf1b6c616af6700443eda227651dfb83a
Enes Yurdatapan <enesyurdatapan@hotmail.com>
2024-10-26 16:26:29 +0300

    26.10.2024 / Explicit Loading & Lazy Loading

---
+++ Lesson24.ExplicitLoading/Lesson24.ExplicitLoading/Program.cs
@@ -0,0 +1,61 @@
+﻿#region Explicit Loading nedir ?
+
+//Oluşturulan sorguya eklenecek verilerin şartlara bağlı bir şekilde/ihtiyaçlara istinaden yüklenmesini sağlayan bir yaklaşımdır.
+
+// ÖRNEK : Id'si 2 olan employee'i getirip ismi "Enes" ise Order verilerini görmek istiyoruz ;
+
+// var employee = await context.Employees.FirstOrDefaultAsync(e=>e.Id==2);
+// if(employee.Name=="Enes")
+//{
+// var orders = await context.Orders.Where(o=>o.EmployeeId==employee.Id).ToListAsync();
+//}
+
+
+// Mantığı bu şekildedir fakat biz bu işlemi daha kolay yöntemlerle yaparız.
+
+
+#endregion
+
+#region Reference
+
+//Explicit Loading sürecinde ilişkisel olarak sorguya eklenmek istenen tablonun navigation propertysi tekil bir tüse bu tabloyu reference ile sorguya ekleyebiliriz.
+
+// var employee = await context.Employees.FirstOrDefaultAsync(e=>e.Id==2);
+//context.Entry(employee).Reference(e=>e.Region).LoadAsync(); >> Bu satırda navigation property verileri eklenmiş oldu.
+#endregion
+
+
+#region Collection
+
+//Explicit Loading sürecinde ilişkisel olarak sorguya eklenmek istenen tablonun navigation propertysi çoğul bir türse bu tabloyu Collection ile sorguya ekleyebiliriz.
+
+// var employee = await context.Employees.FirstOrDefaultAsync(e=>e.Id==2);
+// ..
+// ..
+
+//context.Entry(employee).Collection(e=>e.Orders).LoadAsync();
+
+#endregion
+
+#region Collectionlarda Aggregate Operatör Uygulamak
+
+
+// var employee = await context.Employees.FirstOrDefaultAsync(e=>e.Id==2);
+// ..
+// ..
+
+//await context.Entry(employee).Collection(e=>e.Orders).Query().CountAsync();
+
+
+#endregion
+
+
+#region Collectionlarda Filtreleme
+
+// var employee = await context.Employees.FirstOrDefaultAsync(e=>e.Id==2);
+// ..
+// ..
+
+//await context.Entry(employee).Collection(e=>e.Orders).Query().Where(q=>q.OrderDate.Day==DateTime.Now.Day).ToListAsync();
+
+#endregion
---
+++ Lesson25.LazyLoading/Lesson25.LazyLoading/Program.cs
@@ -0,0 +1,137 @@
+﻿#region Lazy Loading Nedir ?
+
+// Navigation properyler üzerinde bir işlem yapılmaya çalışıldığı taktirde ilgili propertynin temsil ettiği tabloya özel bir sorgu oluşturulup execute edilmesini ve verilerin yüklenmesini sağlayan bir yaklaşımdır.
+
+// var employee = await context.Employees.FindAsync(2);
+// Console.WriteLine(employee.Region.Name);
+
+//senaryo budur.
+
+#endregion
+
+#region Proxy'lerle Lazy Loading
+
+//Microsoft.EntityFrameworkCore.Proxies kütüphanesi
+
+// protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//{
+// optionsBuilder.UseLazyLoadingProxies();
+
+//}
+
+
+#endregion
+
+
+#region Propertylerin Virtual olması
+
+// Eğer ki proxyler üzerinden lazy loading operasyonu gerçekleştiriyorsanız navigation propertylerin virtual ile işaretlenmesi gerekir.
+
+#endregion
+
+#region Proxy olmaksızın Lazy Loading
+
+//Proxyler tüm platformlarda desteklenmeyebilir. Böyle bir durumda manuel bir şekilde lazy loadingi uygulamak mecburiyetinde kalabiliriz.
+
+
+#region ILazyLoader Interface'i ile Lazy Loading
+
+//Microsoft.EntityFrameworkCore.Abstraction
+
+//Manuel yapılan Lazy Loading operasyonlarında Navigation Propertylerin virtual ile işaretlenmesine gerek yoktur
+
+using System.Runtime.CompilerServices;
+
+class Employee
+{
+  //  ILazyLoader _lazyLoader;
+    Region _region;
+    //public Employee()
+    //{
+        
+    //}
+    public Employee(/*ILazyLoader lazyLoader*/)
+    {
+        //_lazyLoader = lazyLoader;
+    }
+
+   // public  Region Region { get => _lazyLoader.Load(this, ref _region); set => _region =value; }
+}
+class Region
+{
+    //  ILazyLoader _lazyLoader;
+    ICollection<Employee> _employees;
+
+    //public Region()
+    //{
+
+    //}
+    public Region(/*ILazyLoader lazyLoader*/)
+    {
+        //_lazyLoader = lazyLoader;
+    }
+    //public ICollection<Employee> Employees { get => _lazyLoader.Load(this, ref _employees); set => _employees = value; }
+}
+
+
+#endregion
+
+#region Delegate ile Lazy Loading
+
+
+
+class Employee2
+{
+    Action<object, string> _lazyLoader;
+    Region _region;
+    //public Employee()
+    //{
+
+    //}
+    public Employee2(Action<object, string> lazyLoader)
+    {
+        _lazyLoader = lazyLoader;
+    }
+
+    // public  Region Region { get => _lazyLoader.Load(this, ref _region); set => _region =value; }
+}
+class Region2
+{
+    Action<object, string> _lazyLoader;
+    ICollection<Employee> _employees;
+
+    //public Region()
+    //{
+
+    //}
+    public Region2(Action<object, string> lazyLoader)
+    {
+        _lazyLoader = lazyLoader;
+    }
+    //public ICollection<Employee> Employees { get => _lazyLoader.Load(this, ref _employees); set => _employees = value; }
+}
+
+static class LazyLoadingExtension
+{
+    public static TRelated Load<TRelated>(this Action<object,string> loader, object entity, ref TRelated navigation, [CallerMemberName]string navigationName=null)
+    {
+        loader.Invoke(entity,navigationName);
+        return navigation;
+    }
+}
+
+
+#endregion
+
+#endregion
+
+#region N+1 Problemi
+
+//var region = await context.Regions.FindAsync(1);
+//foreach (var employee in region.Employees)
+//{
+//    Console.WriteLine(order.OrderDate);
+//}
+
+// Lazy Loading kullanım açısından oldukça maliyetli ve performans düşürücü bir etkiye  sahip yöntemdir. O yüzden kullanırken mümkün oldukça dikkatli olmalı ve özellikle navigation propertylerin döngüsel tetikleme durumlarında lazy loadingi tercih etmemeye odaklanmalıyız. Aksi taktirde her bir tetiklemeye karşılık aynı sorguları üretip execute edecektir. Bu durumu N+1 problemi olarak adlandırırız.
+#endregion
