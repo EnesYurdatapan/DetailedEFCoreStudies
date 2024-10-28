dae0e069ba3b01d862adcc4d1c66e62973c0c4bd
Enes Yurdatapan <enesyurdatapan@hotmail.com>
2024-10-28 16:58:02 +0300

    28.10.2024 / Complex Query Operations && SQL Queries

---
+++ Lesson26.ComplexQueryOperators/Lesson26.ComplexQueryOperators/Program.cs
@@ -0,0 +1,241 @@
+﻿
+using System.Globalization;
+
+class Photo
+{
+    public int PersonId { get; set; }
+    public string Url { get; set; }
+    public Person Person { get; set; }
+}
+public enum Gender { Man, Woman }
+class Person
+{
+    public int PersonId { get; set; }
+    public string Name { get; set; }
+    public Gender Gender { get; set; }
+    public Photo Photo { get; set; }
+    public ICollection<Order> Order { get; set; }
+}
+class Order
+{
+    public int OrderId { get; set; }
+    public int PersonId { get; set; }
+    public string Description { get; set; }
+    public Person Person { get; set; }
+}
+
+
+#region Join
+
+#region Query Syntax
+
+// var query = from photo in context.Photos
+// join person in context.Persons
+// on photo.PersonId equals person.PersonId
+// select new
+// {
+//      person.Name,
+//       photo.Url
+// };
+// query.ToListAsync();
+#endregion
+
+#region Method Syntax
+
+// var query = context.Photos.Join(context.Persons, photo=>photo.PersonId,person=>person.PersonId,(photo,person) => new { person.Name, photo.Url });
+//var datas = await query.ToListAsync();
+
+#endregion
+
+#region Multiple Columns Join
+
+#region  Query Syntax
+
+// var query = from photo in context.Photos
+// join person in context.Persons
+// on new{ photo.PersonId, photo.Url }  equals new{ person.PersonId, Url = person.Name }
+// select new
+// {
+//      person.Name,
+//       photo.Url
+// };
+//var datas = await query.ToListAsync();
+
+#endregion
+
+#region Method Syntax
+
+// var query = context.Photos.Join(context.Persons, photo=>new{ photo.PersonId, photo.Url },person=>new{ person.PersonId, Url = person.Name },(photo,person) => new { person.Name, photo.Url });
+//var datas = await query.ToListAsync();
+
+#endregion
+
+#endregion
+
+#region 2'den Fazla Tabloyla Join
+
+#region Query Syntax
+//var query = from photo in context.Photos
+//  join person in context.Persons
+//  on photo.PersonId
+//  equals person.PersonId
+//  join order in Context.Orders
+//  on person.PersonId
+//  equals order.PersonId
+//  select new
+//  {
+//      person.Name,
+//      photo.Url,
+//      order.Description
+//  }; 
+// var datas = await query.ToListAsync();
+
+#endregion
+
+#region Method Syntax
+
+// var query = context.Photos.Join(context.Persons, photo=>photo.PersonId,person=>person.PersonId,(photo,person) => new { person.PersonId, person.Name, photo.Url }).Join(context.Orders, oncekiSorgu=> oncekiSorgu.PersonId, order=> order.PersonId, (oncekiSorgu, order) => new { oncekiSorgu.Name, oncekiSorgu.Url, order.Description });
+// var data = await query.ToListAsync();
+
+#endregion
+
+#endregion
+
+#region Grup Join
+
+// var query = from photo in context.Photos
+// join order in context.Orders
+// on photo.PersonId equals order.PersonId into personOrders
+// select new
+// {
+//      person.Name,
+//       Count = personOrders.Count(),
+//       personOrders
+// };
+// query.ToListAsync();
+
+//Orderları tekil olarak değil de person ile grup olarak elde ettik
+#endregion
+
+#endregion
+
+#region Left Join
+
+//DefaultIfEmpty : Sorgulama sürecinde ilişkisel olarak karşılığı olmayan verilere default değerini yazdıran yani LEFT JOIN sorgusunu oluşturtan bir fonksiyondur
+
+// var query = from person in context.Persons
+// join order in context.Orders
+// on person.PersonId equals order.PersonId into personOrders
+// from order in persoOrders.DefaultIfEmpty()
+// select new
+// {
+//      person.Name,
+//      order.Description
+// };
+// query.ToListAsync();
+
+#endregion
+
+#region Right Join
+
+// Left joinin tam tersi olarak sorgudaki tabloların yerlerinin değiştirilmesi yeterlidir
+
+
+// var query = from order in context.Orders
+// join person in context.Persons
+// on order.PersonId equals person.PersonId into orderPersons
+// from person in orderPersons.DefaultIfEmpty()
+// select new
+// {
+//      person.Name,
+//       order.Description
+// };
+// query.ToListAsync();
+
+#endregion
+
+#region Full Join
+
+// var leftQuery = from person in context.Persons
+// join order in context.Orders
+// on person.PersonId equals order.PersonId into personOrders
+// from order in persoOrders.DefaultIfEmpty()
+// select new
+// {
+//      person.Name,
+//      order.Description
+// };
+
+
+// var rightQuery = from order in context.Orders
+// join person in context.Persons
+// on order.PersonId equals person.PersonId into orderPersons
+// from person in orderPersons.DefaultIfEmpty()
+// select new
+// {
+//      person.Name,
+//       order.Description
+// };
+
+// var fullJoin = leftQuery.Union(rightQuery);
+
+//var datas = await fullJoin.ToListAsync();
+
+#endregion
+
+#region Cross Join
+
+//SQL'de CROSS JOIN, iki tablo arasında her satırın diğer tabloya ait her satır ile eşleşmesini sağlar
+// CROSS JOIN, genellikle diğer JOIN türleri gibi ilişkili veri elde etmekten ziyade tüm olası kombinasyonları görmek için kullanılır.
+//Örneğin A tablosunda 3 satır ve B tablosunda 2 satır varsa toplam 3x2 = 6 satırlık veri oluşur.
+
+// EF Core'da from üzerine from yazdığımızda bunun bir Cross Join olduğunu anlar.
+
+// var query = from order in context.Orders
+// from person in context.Persons
+// select new
+// {
+//      order,
+//      person
+// };
+// query.ToListAsync();
+
+#endregion
+
+#region Collection Selector'da Where Kullanma Durumu
+
+// EF bu durumda bir inner join sorgusu oluşturur.
+
+// var query = from order in context.Orders
+// from person in context.Persons.Where(p=>p.PersonId==order.PersonId)
+// select new
+// {
+//      order,
+//      person
+// };
+
+#endregion
+
+#region Cross Apply
+
+// var query = from order in context.Orders
+// from person in context.Persons.Select(p=>order.Description)
+// select new
+// {
+//      order,
+//      person
+// };
+
+#endregion
+
+#region Outer Apply
+
+// var query = from order in context.Orders
+// from person in context.Persons.Select(p=>order.Description).DefaultIfEmpty();
+// select new
+// {
+//      order,
+//      person
+// };
+
+#endregion
---
+++ Lesson27.SQLQueries/Lesson27.SQLQueries/Program.cs
@@ -0,0 +1,116 @@
+﻿//Eğer ki sorguyu LINQ ile ifade edemiyorsak, veya LINQ'in ürettiği sorguya nazaran daha optimize bir sorguyu manuel geliştirmek ve EF Core üzerinden execute etmek istersek EF Core bu davranışı destekler.
+//Tarafımızca oluşturulmuş sorguları EF Core tarafından execute edebilmek için o sorgunun sonucunu karşılayacak bir entity modelinin tasarlanmış ve bunun DbSet olarak context nesnesine tanımlanmış olması gerekiyor.
+
+using System.Reflection;
+
+class Person
+{
+    public int PersonId { get; set; }
+    public string Name { get; set; }
+    public ICollection<Order> Order { get; set; }
+}
+class Order
+{
+    public int OrderId { get; set; }
+    public int PersonId { get; set; }
+    public string Description { get; set; }
+    public Person Person { get; set; }
+}
+
+#region FromSqlInterpolated
+
+//EF Core 7.0 sürümünden önce ham sorguları execute edebildiğimiz fonksiyondur.
+//var persons = await context.Persons.FromSqlInterpolated($"SELECT * FROM Persons").ToListAsync();
+
+#endregion
+
+#region FromSql
+
+//EF Core 7.0 ile gelen metottur. FromSqlInterpolated ile aynı işlevi görür.
+
+#region Query Execute
+
+//var persons = await context.Persons.FromSql($"SELECT * FROM Persons").ToListAsync();
+
+#endregion
+
+#region Stored Procedure Execute
+
+//var persons = await context.Persons.FromSql($"EXECUTE dbo.sp_GetAllPersons NULL").ToListAsync();
+
+
+#endregion
+
+#region Parametreli Sorgu Oluşturma
+
+//------------------- Örnek 1 -------------------------
+
+//int personId=3;
+//var persons = await context.Persons.FromSql($"SELECT * FROM Persons Where PersonId={personId}").ToListAsync();
+
+//Burada sorguya geçirilen personId değişkeni arka planda bir DbParameter türüne dönüştürülerek o şekilde sorguya dahil edilir.
+
+// ------------------------- Örnek 2 ----------------------------------
+
+//var persons = await context.Persons.FromSql($"EXECUTE dbo.sp_GetAllPersons @PersonId = {personId}").ToListAsync();
+
+
+//--------------------------- Örnek 3 ------------------------------------------
+
+//SqlParameter personId = new("PersonId","3");
+//personId.DbType= System.Data.DbType.Int32;
+//personId.Direction= System.Data.ParameterDirection.Input;
+//var persons = await context.Persons.FromSql($"SELECT * FROM Persons Where PersonId={personId}").ToListAsync();
+
+//Bu örnekte kendimiz SqlParameter olarak verdiğimiz için dönüşüme gerek kalmaz. Ufak da olsa performans optimizasyonu sağlar.
+
+
+#endregion
+
+#endregion
+
+#region Dynamic SQL Oluşturma ve Parametre Girme - FromSqlRaw
+
+//string columnName= "PersonId" , value="3";
+//var persons = await context.Persons.FromSql($"SELECT * FROM Persons Where {columnName} = {value}").ToListAsync();
+
+//Yukardaki kod ÇALIŞMAZ !
+//Ef Core dinamik olarak oluşturulan sorgularda özellikle kolon isimleri parametreleştirilmişsse o sorguyu çalıştırmaz !
+
+//string columnName= "PersonId";
+//SqlParameter value = new("PersonId","3");
+//var persons = await context.Persons.FromSqlRaw($"SELECT * FROM Persons Where {columnName} = @PersonId",value).ToListAsync();
+
+//FromSql ve FromSqlInterpolated metotlarında SQL injection vs gibi güvenlik önlemleri alınmış vaziyettedir. Lakin dinamik olarak sorguları oluşturuyorsanız eğer burada güvenlik geliştirici tarafından sağlanmalıdır. Yani gelen sorguda yorumlar, noktalı virgüller veya SQL'e özel karakterlerin algılanması ve bunların temizlenmesi geliştirici tarafından sağlanır.
+
+#endregion
+
+#region SqlQuery - Entity olmayan Scalar Sorguların Çalıştırılması - Non Entity
+
+//Entitysi olmayan scalar sorguların çalıştırılıp sonucunu elde etmeemizi sağlayan yeni bir fonksiyondur.
+//var data = await context.Database.SqlQuery<int>($"SELECt PersonId FROM Persons").ToListAsync();
+
+//SqlQuery'de LINQ operatörleriyle sorguya ekstradan katkıda bulunmak istiyorsanız eğer bu sorgu neticesinde gelecek olan kolonun adını Value olarak bildirmeniz gerekir. Çünkü, SqlQuery sorguyu bir subquery olarak generate etmektedir. Haliyle bu durumdan dolayı LINQ ile verilen şart ifadeleri statik olarak Value kolonuna göre tasarlanmıştır. O yüzden b şekilde bir çalışma zorunlu hale gelmektedir.
+//var data = await context.Database.SqlQuery<int>($"SELECt PersonId Value FROM Persons").Where(x=>x>5).ToListAsync();
+
+
+#endregion
+
+
+#region ExecuteSql
+
+// context.Database.ExecuteSqlAsync($"UPDATE Persons SET Name='Fatma' WHERE PersonId="1");
+
+#endregion
+
+#region Sınırlılıklar
+
+//Queryler entity türünün tüm özellikleri için kolonlarda değer döndürmelidir.
+//var persons = await context.Persons.FromSql($"SELECT Name FROM Persons").ToListAsync(); >> Sadece Name döndürüldüğü için hata verir.
+
+// Sütun isimleri property isimleriyle aynı olmalıdır.
+
+//SQL sorgusu Join yapısı içeremez !! Bu tarz ihtiyaç noktalarında include fonksiyonu kullanılmalıdır.
+
+
+#endregion
