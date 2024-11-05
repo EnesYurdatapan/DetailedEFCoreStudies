7142471d4c552594a1a707b511bc39ec8994c6e6
Enes Yurdatapan <enesyurdatapan@hotmail.com>
2024-11-06 00:00:44 +0300

    05.11.2024 / QueryLogs && QueryTags

---
+++ Lesson34.QueryLog/Lesson34.QueryLog/Program.cs
@@ -0,0 +1,31 @@
+﻿#region Query Log Nedir? 
+
+//LINQ sorguları neticesinde generate edilen sorguları izleyebilmek ve olası teknik hataları ayıklayabilmek amacıyla query log mekanizmasından istifade etmekteyiz.
+
+#endregion
+
+#region Nasıl konfigüre edilir ?
+
+//Microsoft.Extension.Logging.Console  kütüphanesi(Console harici alternatifleri de bulunmakta.)
+
+
+class Context
+{
+// readonly ILoggerFactory loggerFactory = LoggerFactory.Create(builder=>builder.AddConsole());
+
+
+// OnConfiguring
+// {.....
+//          optionsBuilder.UseLoggerFactory(loggerFactory);
+// ...}
+}
+
+// await context.Persons.Where(...);  örneğin bu sorgu neticesinde oluşturulan SQL querysi console'a loglanır.
+#endregion
+
+#region Filtreleme nasıl yapılır ?
+
+// readonly ILoggerFactory loggerFactory = LoggerFactory.Create(builder=>builder.AddFilter((category,level)=>{ return category == DbLoggerCategory.Database.Command.Name && level == LogLevel.Information;})).AddConsole());
+
+
+#endregion
---
+++ Lesson35.QueryTags/Lesson35.QueryTags/Program.cs
@@ -0,0 +1,31 @@
+﻿#region QueryTags nedir?
+
+//EF Core ile generate edilen sorgulara açıklama eklememizi sağlayarak; SQL Profiler, Query Log vs gibi yapılarda bu açıklamalar eşliğinde sorguları gözlemlememizi sağlayan bir özelliktir.
+
+
+#endregion
+
+#region TagWith Metodu
+
+//ToList ile sorgu execute edilmeden önce çağırılıp sorguya eklenir. 
+
+//await context.Persons.TagWith("Örnek açıklama...").ToListAsync();
+
+#endregion
+
+#region Multiple TagWith
+
+//Bir sorguya birden fazla açıklama eklenebilir.
+//await context.Persons.TagWith("Örnek açıklama...").Include(p=>p.Orders).TagWith("Personeller ilişkilendirildi...").ToListAsync();
+
+
+#endregion
+
+#region TagWithCallSite Metodu
+
+//Oluşturulan sorguya açıklama satırı ekler, ek olarak bu sorgunun bu .cs dosyasında hangi satırda üretildiği bilgisini de verir.
+
+//await context.Persons.TagWithCallSite("Örnek açıklama...").ToListAsync();
+
+
+#endregion
