84e433430eb2f0baf9b6ad8d0e9afb73ca0e2540
Enes Yurdatapan <enesyurdatapan@hotmail.com>
2024-11-12 18:06:39 +0300

    12.11.2024 / Global Query Filters && Owned Entity Types

---
+++ Lesson36.GlobalFilters/Lesson36.GlobalFilters/Program.cs
@@ -0,0 +1,37 @@
+﻿#region Global Query Filters Nedir?
+
+// Bir entity'e özel uygulama seviyesinde genel/ön kabullu şartlar oluşturmamızı sağlayan ve böylece verileri global bir şekilde filtrelememizi sağlayan bir özelliktir.
+
+//Böylece belirtilen entity üzerinde yapılan tüm sorgulamalarda ekstradan bir şart ifadesine gerek kalmaksızın filtreleri otomatik uygulayarak hızlıca sorgulama yapmamızı sağlamaktadır.
+
+//Genellikle uygulama bazında IsActive gibi verilerle çalışıldığı durumlarda,
+//MultiTenancy uygulamalarda TenantId tanımlarken vs. kullanılabilir.
+#endregion
+
+#region Global Query Filters Nasıl Uygulanır ?
+
+
+
+// modelBuilder.Entity<Person>.HasQueryFilter(p=>p.IsActive);
+
+#endregion
+
+#region Navigation Property üzerinden global query filter
+
+// modelBuilder.Entity<Person>.HasQueryFilter(p=>p.Orders.Count>0);
+
+
+#endregion
+
+#region Global Query Filters nasıl ignore edilir ?
+
+//var person = await context.Persons.IgnoreQueryFilters().ToListAsync();
+
+#endregion
+
+#region Dikkat edilmesi gereken husus!
+
+//Global Query Filter uygulanan kolona farkında olmaksızın şart uygulanabilmektedir. Bu duruma dikkat edilmelidir.
+//Yani globalde verilen filtre unutulup kod esnasında tekrar yazılabilme durumu.
+
+#endregion
---
+++ Lesson37.OwnedEntityTypes/Lesson37.OwnedEntityTypes/Program.cs
@@ -0,0 +1,120 @@
+﻿#region Owned Entity Types Nedir ?
+
+// EF Core entity sınıflarını parçalayarak propertylerini kümesel olarak farklı sınıflarda barındırmamızı ve tüm bu sınıfları ilgili entity'de birleştirip bütünsel olarak çalışmamıza izin vermektedir.
+// Böylece bir entity, sahip olunan(owned) birden fazla alt sınıfın birleşmesiyle meydana gelebilmektedir.
+
+
+//Domain Driven Design yaklaşımında Value Object'lere karşılık olarak Owned Entity Types'lar kullanılır.
+#endregion
+
+#region Owned Entity Types Nasıl Uygulanır ?
+
+//Normal bir entityde farklı sınıfların referans edilmesi primary key vs. gibi hatalara sebebiyet verir. Çünkü direkt sınıfın referans olarak alınması ef core tarafından ilişkisel bir tasarım olarak algılanır. Bizlerin entity içerisindeki propertyleri kümesel olarak barındıran sınıfları o entitynin bir parçası olduğunu bildirmemiz gerekir.
+
+class Employee
+{
+    public int Id { get; set; }
+    //public string Name { get; set; }
+    //public string MiddleName { get; set; }
+    //public string LastName { get; set; }
+    //public string StreetAddress { get; set; }
+    //public string Location { get; set; }
+    public bool IsActive { get; set; }
+    public EmployeeName EmployeeName { get; set; }
+    public Address Address { get; set; }
+}
+
+class EmployeeName
+{
+    public string Name { get; set; }
+    public string MiddleName { get; set; }
+    public string LastName { get; set; }
+}
+
+class Address
+{
+    public string StreetAddress { get; set; }
+    public string Location { get; set; }
+}
+
+#region OwnsOne Metodu
+
+//modelBuilder.Entity<Employee>.OwnsOne(e=>e.EmployeeName, builder=>builder.Property(e=>e.Name).HasColumnName("Name"));
+//modelBuilder.Entity<Employee>.OwnsOne(e=>e.Address);
+
+#endregion
+
+#region Owned Attribute
+
+//[Owned]
+//class EmployeeName
+//{
+//    public string Name { get; set; }
+//    public string MiddleName { get; set; }
+//    public string LastName { get; set; }
+//}
+
+//[Owned]
+//class Address
+//{
+//    public string StreetAddress { get; set; }
+//    public string Location { get; set; }
+//}
+
+#endregion
+
+#region  IEntityTypeConfiguration<T>
+
+//class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+//{
+//    public void Configure(EntityTypeBuilder<Employee> builder)
+//    {
+//        //builder.OwnsOne(e=>e.EmployeeName, builder=>builder.Property(e=>e.Name).HasColumnName("Name"));
+//        //builder.Entity<Employee>.OwnsOne(e=>e.Address);
+//    }
+//}
+
+//OnModelCreating fonksiyonu içinde : 
+//    modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+
+
+#endregion
+
+#region OwnsMany metodu
+
+
+//Entitynin farklı özelliklerine başka bir sınıftan ICollection türünde Navigation Property aracılığı ile ilişkisel olarak erişebilmemizi sağlayan bir işleve sahiptir.
+//Normalde Has ilişkisi olarak kurulabilecek bu ilişkinin temel farkı, Has ilişkisi DbSet propertysi gerektirirken, OwnsMany metodu ise DbSet'e ihtiyaç duymaksızın gerçekleştirmemizi sağlar.
+
+class Employee2
+{
+    public ICollection<Order> Orders { get; set; }
+}
+class Order
+{
+    public string OrderDate { get; set; }
+    public int Price { get; set; }
+    public int MyProperty { get; set; }
+    public Employee2 Employee { get; set; }
+}
+
+//modelBuilder.Entity<Employee>().OwnsMany(e=>e.Orders, builder=>
+//{
+//builder.WithOwner().HasForeginKey("OwnedEmployeeId");
+//builder.Property<int>("Id");
+//builder.HasKey("Id");
+//};
+
+#endregion
+
+#endregion
+
+#region Sınırlılıklar
+
+//Herhangi bir owned entity type için DbSet propertysine ihtiyaç yoktur.
+//OnModelCreating fonksiyonunda Entity<T> metodu ile Owned Entity Type türünden bir sınıf üzerinde herhangi bir konf. gerçekleştirilemez.
+
+//Owned Entity Type'ların kalıtımsal hiyerarşi desteği yoktur.
+
+#endregion
+
