82bc9ceeefe959f0b87a19246a13df38f92be248
Enes Yurdatapan <enesyurdatapan@hotmail.com>
2024-11-26 16:41:12 +0300

    26.11.2024 / Connection Resiliency & Data Concurrency

---
+++ Lesson39.ConnectionResiliency/Lesson39.ConnectionResiliency/Program.cs
@@ -0,0 +1,106 @@
+﻿#region Connection Resiliency Nedir ?
+
+//EF Core üzerinde yapılan veritabanı çalışmaları sürecinde ister istemez veritabanı bağlantısında kopuşlar/ kesintiler vs. meydana gelebilir.
+// Connectiob Resiliency ile kopan bağlantıyı tekrar kurmak için gerekli bağlantı taleplerinde bulunabilir  ve bir yandan da execution strategy dediğimiz davranış modellerini belirleyerek bağlantıların kopması durumunda tekrar edecek olan sorguları baştan sona yeniden tetikleyebiliriz.
+
+#endregion
+
+#region EnableRetryOnFailure
+
+//Uygulama sürecinde veritabanı bağlantısı koptuğu taktirde bu yapılandırma sayesinde bağlantıyı tekrardan kurmaya çalışabiliriz.
+
+// OnConfiguring
+// { 
+//      optionsBuilder.UseSqlServer("...ConnectionString..., builder=> builder.EnableRetryOnFailure());
+//
+//
+//  }
+
+#region MaxRetryCount
+
+//Yeniden bağlantı sağlanmasını deneme durumunun kaç kere gerçekleştirileceğini bildirmektedir.
+//Default değeri 6'dır
+#endregion
+
+#region MaxRetryDelay
+
+// Yeniden bağlantı sağlanmasını deneme periyodunu bildirmektedir.
+//Default Değeri 30.
+
+#endregion
+
+//      optionsBuilder.UseSqlServer("...ConnectionString..., builder=> builder.EnableRetryOnFailure( maxRetryCount : 5, maxRetryDelay : TimeSpan.FromSeconds(15), errorNumbersToAdd : new[] {4060}));
+
+
+
+#endregion
+
+#region Execution Strategies
+
+// EF Core ile yapılan bir işlem vt bağlantısı sebebiyle başarısız olduğu taktirde yeniden bağlantı denenirken yapılan davranışa ExecutionStrategy denmektedir. Az Önce yaptığımız bir Execution Strategy idi.
+// Bu stratejiyi default değerlerle kullanabileceğmiz gibi custom olarak de kendimize göre özelleştirebilir ve bağlantı koptuğu durumlarda istediğimiz aksiyonları alabiliriz.
+
+#region Default Execution Strategy
+// Eğer ki EnableRetryOnFailure metodunu kullanıyorsak  bu default execution strategy'e karşılık gelecektir.
+
+
+#endregion
+
+#region Custom Execution Strategy
+
+
+class CustomExecutionStrategy /*: ExecutionStrategy*/
+{
+    //public CustomExecutionStrategy(..........)
+    //{
+    //       ....... 
+    //}
+
+    //public CustomExecutionStrategy(.......)
+    //{
+    //      .........  
+    //}
+
+    //protected override bool ShouldRetryOn(Exception exception)
+    //{
+                // Yeniden bağlantı durumunun söz konusu olduğu anlarda yapılacak işlemler...
+                //Console.Writeline("Bağlantı tekrar kuruluyor...");
+    //}
+}
+
+class Context
+{
+    // OnConfiguring
+    // { 
+    //      optionsBuilder.UseSqlServer("...ConnectionString..., builder=> builder.ExecutionStrategy(dependencies => new CustomExecutionStrategy(dependencies, 3 ,TimeSpan.FromSeconds(15)))));
+    //
+    //
+    //  }
+}
+
+#endregion
+
+#region Bağlantı koptuğu anda execute edilmesi gereken tüm çalışmaları tekrar işlemek
+
+//EF Core ile yapılan çalışma sürecinde veritabanı bağlantısının kesilmesi durumlarında, bazen bağlantının tekrardan kurulması tek başına yetmemekte, kesintinin olduğu çalışmanın da baştan tekrardan işlenmesi gerekebilmektedir. İşte bu tarz durumlara karşılık EF Core Execute - ExecuteAsync fonksiyonunu bizlere sumaktadır.
+
+//Execute fonksiyonu, içerisine vermiş olduğumuz kodları commit edilene kadar işleyecektir. Eğer ki bağlantı kesilmesi meydana gelirse, bağlantının tekraran kurulması durumunda Execute içerisindeki çalışmalar tekrar baştan işlenecek ve böylece yapılan işlemin tutarlılığı için gerekli çalışma sağlanmış olunacaktır.
+
+// var strategy = context.Database.CreateExecutionStrategy();
+//await strategy.ExecuteAsync(async () => 
+// {
+//      using var transaction = await context.Database.BeginTransactionAsync();
+//      await context.Persons.AddAsync(new() { Name="Hilmi" });
+//      await context.SaveChangesAsync();
+
+//await transaction.CommitAsync();
+
+#endregion
+
+#region Execution Strategy hangi durumlarda kullanılır ?
+
+// Veritabanının şifresi belirli periyotlarda otomatik olarak değişen uygulamalarda güncel şifreyle connection string'i sağlayacak bir operasyonu custom execution strategy belirleyerek gerçekleştirebilirsiniz.
+
+#endregion
+
+#endregion
---
+++ Lesson40.DataConcurrency/Lesson40.DataConcurrency/Program.cs
@@ -0,0 +1,102 @@
+﻿#region Data Concurrency Nedir ?
+
+// Geliştirdiğimiz uygulamalarda zaman zaman verisel olarak tutarsızlıklar meydana gelebilmektedir.
+// Örneğin ; birden fazla uygulamanın veya client'ın aynı veritabanı üzerinde eşzamanlı olarak çalışıtğı durumlarda verisel anlamda uygulamadan uygulamaya yahut client'tan clienta tutarsızlıklar meydana gelebilir.
+
+// Data Concurrency kavramı, uygulamalardaki veri tutarsızlığı durumlarına karşılık yönetilebilirlği sağlayacak olan davranışları kapsayan bir kavramdır.
+
+//Bir uygulamada veri tutarsızlığının olması demek, o uygulamayı kullanan kullanıcıları yanıltmak demektir.
+
+//Veri tutarsızlığınını olduğu uygulamalarda istatistiksel olarak yanlış sonuçlar elde edilebilir.
+
+#endregion
+
+#region Stale & Dirty Data Nedir ?
+
+//Stale Data : Veri tutatsızlığına sebebiyet verebilecek güncellenmemiş veya zamanı geçmiş olan verileri ifade eder.
+// Örneğin bir ürünün stok durumu sıfırlandığı halde arayüz üzerinde bunu ifade eden bir güncelleme durumu söz konusu değilse işte bu stale data durumuna bir örnektir.
+
+//Dirty Data : Veri tutatsızlığına sebebiyet verebilecek verinin hatalı yahut yanlış olduğunu ifade etmektedir. 
+// Örneğin adı 'Ahmet' olan bir kullanıcının veritabanında 'Mehmet' olarak tutulması dirty data örneklendirmesidir.
+
+#endregion
+
+#region Last In Wins (Son Gelen Kazanır)
+
+// Bir veri yapısında son yapılan aksiyona göre en güncel verinin en üstte bulunmasını ifade eden bir deyimsel terimdir.
+
+#endregion
+
+#region Pessimistic Lock (Kötümser Kilitleme)
+
+// Bir transaction sürecinde elde edilen veriler üzerinde farklı sorgularla değişiklik yapılmasını engellemek için ilgili verilerin kilitlenmesini sağlayarak değişikliğe karşı direnç oluşturulmasını ifade eden bir yöntemdir.
+
+//Bu verilerin kilitlenmesi durumu ilgili transactionın commit ya da rollback edilmesi ile sınırlıdır.
+
+
+#region Deadlock Nedir ?
+
+//Kitlenmiş olan bir verinin veritabanı seviyesinde meydana gelen sistemsel bir hatadan dolayı kilidinin çözülememesi yahut döngüsel olarak kilitlenme durumunun meydana gelmesini ifade eden bir terimdir.
+
+//Pessimistic Lock yönteminde deadlock durumunu yaşamanız bir ihtimaldir. O yüzden değerlendirilmesi gereken ve iyi düşünülerek tercih edilmesi gereken bir yaklaşımdır.
+
+#endregion
+
+
+#region WITH (XLOCK)
+
+//await using var transaction = await context.Database.BeginTransactionAsync();
+
+// context.Persons.FromSql($"SELECT * FROM Persons WITH (XLOCK) WHERE PersonId=5");
+
+//await transaction.CommitAsync(); >> Commit edildiği için Lock'u salar.
+
+#endregion
+
+
+#endregion
+
+#region Optimistic Lock (İyimse Kilitleme)
+
+//Bir verinin stale olup olmadığını anlamak için herhangi bir locking işlemi olmaksızın versiyon mantığında çalışmamızı sağlayan yaklaşımdır.
+//Optimistic Lock yönteminde, Pessimistic lock'da olduğu gibi veriler üzerinde tutarsızlığa sebep olabilecek değişiklikler fiziksel olarak engellenmemektedir. Yani veriler tutarsızlığı sağlayacak şekilde değişitirlebilir.
+//Fakat Optimistic Lock yaklaşımı ile bu veriler üzerindeki tutarsızlık durumunu takip edebilmek için versiyon bilgisini kullanıyoruz.
+//Her bir veriye karşılık bir versiyon bilgisi üretiliyor. Bu bilgi ister metinsel ister sayısal olabilir. Bu versiyon bilgisi veri üzerinde yapılan her bir değişiklik neticesinde güncellenecektir. Dolayısıyla bu güncellemeyi kolay bir şekilde gerçekleştirebilmek için sayısal olmasını tercih ederiz.
+// EF Core üzerinden verileri sorgularken ilgili verilerin versiyon bilgilierini de in-memory'e alıyoruz. Ardından veri üzerinde bir değişiklik yapılırsa bu in-memorydeki versiyon bilgisi ile veritabanındaki versiyon bilgisi karşılaştırılır. Eğerki bu karşılaştırma doğrulanıyorsa yapılan aksiyon geçerli olacaktır, yok eğer doğrulanmıyorsa demek ki verinin değeri değişmiş anlamına gelecek yani bir tutarsızlık durumu olduğu anlaşılacaktır. İşte bu durumda bir hata fırlatılacak ve aksiyon gerçekleştirilmeyecektir.
+
+//EF Core Optimistic Lock yaklaşımı için genetiğinde yapısal bir özellik barındırmaktadır.
+
+#region Property Based Configuration
+
+//Verisel tutarlılığın kontrol edilmek istendiği propertyler ConcurrencyCheck attribute ile işaretlenir. Bu işaretleme neticesinde her entity'nin instance'ı için in-memoryde bir token değeri üretilecektir. Üretilen bu token değeri, alınan aksiyon süreçlerinde EF Core tarafından doğrulanacak ve eğer ki herhangi bir değişiklik yoksa aksiyon başarıyla sonlandırılmış olacaktır.
+// Eğer transaction sürecinde ilgili veri üzerinde (ConcurrencyCheck attribute ü ile işaretlenmiş propertylerde) herhangi bir değişiklik durumu söz konusuysa o taktirde üretilen token da değiştirilecek ve haliyle doğrulama sürecinde geçerli olmayacağı anlaşılacağı için veri tutarsızlığı durumu olduğu anlaşılacak ve hata fırlatılacaktır.
+
+using System.ComponentModel.DataAnnotations;
+
+class Person
+{
+    public int Id { get; set; }
+    [ConcurrencyCheck]
+    public string Name { get; set; }
+}
+
+// VEYA FluentAPI ile 
+//modelBuilder.Entity<Person>().Property(p=>p.Name).IsConcurrencyToken();
+#endregion
+
+#region RowVersion Column
+
+//Bu yaklaşımda ise veritabanındaki her bir satıra karşılık versiyon bilgisi fiziksel olarak oluşturulmaktadır.
+class Person2
+{
+    public int PersonId { get; set; }
+    public string Name { get; set; }
+    [Timestamp]
+    public byte[] RowVersion { get; set; }
+}
+
+//modelBuilder.Entity<Person>().Property(p=>p.RowVersion).IsRowVersion();
+
+#endregion
+
+#endregion
